ef813fcb7e543763a710834eae539bd5e646ca79
P-l <1732895@cegepba.qc.ca>
2019-08-20 11:11:17 -0400

    Rocket-Ajout d'une Fusée et d'un jump

---
+++ Assets/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rbd;
+    public int SPEED = 10000;
+	// Use this for initialization
+	void Start () {
+        rbd = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Jump") != 0)
+        {
+            rbd.AddForce(new Vector3(0, Input.GetAxis("Jump") * Time.deltaTime*SPEED, 0));
+        }
+	}
+}
